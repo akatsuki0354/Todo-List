b9c8dafefe6db1a003199473964b6219fc637a43
Franco Gregorio <117264452+akatsuki0354@users.noreply.github.com>
2022-12-28 23:09:36 +0800

    fibase

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD1GxCsUNfVYuE8vq5Z9u3kj8dHFval7Hc",
-  authDomain: "firestore-9d6f3.firebaseapp.com",
-  databaseURL: "https://firestore-9d6f3-default-rtdb.firebaseio.com",
-  projectId: "firestore-9d6f3",
-  storageBucket: "firestore-9d6f3.appspot.com",
-  messagingSenderId: "937341367464",
-  appId: "1:937341367464:web:a58d03272b5be0a0247e9d",
-  measurementId: "G-9J07C0PJZ7"
+    apiKey: "",
+  authDomain: "",
+  databaseURL: ",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
